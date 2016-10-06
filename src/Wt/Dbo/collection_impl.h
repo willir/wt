36cd0d7578a1224ca9703347b8b13cda16966d9f
Anton Rapetov <willir29@yandex.ru>
2016-10-06 14:28:18 +0300

    Fix bug with NullPointer on comparing (collRes.end() == it)

--- src/Wt/Dbo/collection_impl.h
+++ src/Wt/Dbo/collection_impl.h
@@ -108,7 +108,7 @@ bool collection<C>::iterator::operator== (const iterator& other) const
 {
   return impl_ == other.impl_
     || (!impl_ && other.impl_->ended_)
-    || (impl_->ended_ && !other.impl_);
+    || (!other.impl_ && impl_->ended_);
 }
 
 template <class C>
